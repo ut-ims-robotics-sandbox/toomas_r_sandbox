dbf31bbf0178171139f497244b57342b91efa372
Toomas Roosma <toomas.roosma@gmail.com>
2020-11-18 14:44:53 +0200

    a second ago everything was working

--- master_right/master_right.ino
+++ master_right/master_right.ino
@@ -1,59 +1,61 @@
 #include <Wire.h>
 
 const int addr = 8;
-const int n_bytes = 8;
+const int n_bytes = 4*4;
 byte a, b;
 int16_t bigNum;
 int16_t smallNum;
 
-int voltage[4];
-
+float voltage[4];
 void setup() {
+  voltage[0] = 1.23456789;
   Wire.begin();        // join i2c bus (address optional for master)
   Serial.begin(9600);  // start serial for output
 }
 
 void loop() {
   Wire.requestFrom(addr, n_bytes);    // request 6 bytes from slave device addr
+  String dataString = "";
 
+  unsigned char bytes[4];
+
+  
   while (Wire.available()) { // slave may send less than requested
-    a = Wire.read();
-    b = Wire.read();
-    smallNum = a;
-    bigNum = smallNum << 8 | b;
-//    Serial.print(bigNum);
-//    Serial.print(" ");
-    voltage[0] = bigNum;
+//    a = Wire.read();
+//    b = Wire.read();
+//    smallNum = a;
+//    bigNum = smallNum << 8 | b;
+////    Serial.print(bigNum);
+////    Serial.print(" ");
+//    voltage[0] = bigNum;
 
-    a = Wire.read();
-    b = Wire.read();
-    smallNum = a;
-    bigNum = smallNum << 8 | b;
-//    Serial.print(bigNum);
-//    Serial.print(" ");
-    voltage[1] = bigNum + voltage[0];
-
-    a = Wire.read();
-    b = Wire.read();
-    smallNum = a;
-    bigNum = smallNum << 8 | b;
-//    Serial.print(bigNum);
-//    Serial.print(" ");
-    voltage[2] = bigNum + voltage[1];
-
-    a = Wire.read();
-    b = Wire.read();
-    smallNum = a;
-    bigNum = smallNum << 8 | b;
-//    Serial.print(bigNum);
-//    Serial.print(" ");
-    voltage[3] = bigNum + voltage[2];
+      for(int i=0; i<
+      byte c = Wire.read();
+      byte d = Wire.read();
+      byte e = Wire.read();
+      byte f = Wire.read();
+      
+      //Serial.print(c);
+      //dataString = dataString + c;
+      bytes[0] = c;
+      bytes[1] = d;
+      bytes[2] = e;
+      bytes[3] = f;
   }
+  float x;
+  memcpy(&voltage[0], bytes, sizeof voltage[0]);
+  
+  //Serial.print(dataString);
+  //voltage[0] = dataString.toFloat();
+  //voltage[0] = *(float *)&dataString;
+  
+  Serial.print(" pinge on ");
+  Serial.print(voltage[0], 6);
 
-  for(int i=0; i<4; i++){
-    Serial.print(voltage[i] * (5.0/1023.0) );
-    Serial.print(' ');
-  }
+//  for(int i=0; i<4; i++){
+//    Serial.print(voltage[i] * (5.0/1023.0) );
+//    Serial.print(' ');
+//  }
   Serial.println();
   delay(500);
   
--- slave_left/slave_left.ino
+++ slave_left/slave_left.ino
@@ -2,13 +2,16 @@
 
 const int addr = 8;
 
-int16_t sensor_value0;
-int16_t sensor_value1;
-int16_t sensor_value2;
-int16_t sensor_value3;
+float sensor_value0;
+float sensor_value1;
+float sensor_value2;
+float sensor_value3;
+
+char voltBuff[4];
 
 byte myArray[2];
 
+float voltage[4];
 
 void setup() {
   Wire.begin(addr);                // join i2c bus with address addr
@@ -18,6 +21,8 @@ void setup() {
   pinMode(A1, INPUT);
   pinMode(A2, INPUT);
   pinMode(A3, INPUT);
+
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -29,34 +34,45 @@ void loop() {
 void requestEvent() {
   //master küsib meilt infot, teostame mõõtmised ja vastame talle
 
-  sensor_value0 = analogRead(A0);
-  sensor_value1 = analogRead(A1);
-  sensor_value2 = analogRead(A2);
-  sensor_value3 = analogRead(A3);
+  sensor_value0 = analogRead(A0) * 5.0/1023.0;
+  sensor_value1 = analogRead(A1) * 5.0/1023.0;
+  sensor_value2 = analogRead(A2) * 5.0/1023.0;
+  sensor_value3 = analogRead(A3) * 5.0/1023.0;
 
-//  sensor_value0 = 1023;
-//  sensor_value1 = 103;
-//  sensor_value2 = 800;
-//  sensor_value3 = 987;
+  //do conversions
 
+  voltage[0] = sensor_value0;
+  voltage[1] = sensor_value1;
+  voltage[2] = sensor_value2;
+  voltage[3] = sensor_value3;
 
-  myArray[0] = (sensor_value0 >> 8) & 0xFF;
-  myArray[1] = sensor_value0 & 0xFF;
-  Wire.write(myArray, 2);
+  voltage[0] = 1.23456;
 
-  myArray[0] = (sensor_value1 >> 8) & 0xFF;
-  myArray[1] = sensor_value1 & 0xFF;
-  Wire.write(myArray, 2);
+  for(int i=0; i<4; i++){
+    for(int j=0; j<4; j++){
+      Wire.write(*((unsigned char*)&voltage[i] + j));
+    }    
+  }
+
   
-  myArray[0] = (sensor_value2 >> 8) & 0xFF;
-  myArray[1] = sensor_value2 & 0xFF;
-  Wire.write(myArray, 2);
-  
-  myArray[0] = (sensor_value3 >> 8) & 0xFF;
-  myArray[1] = sensor_value3 & 0xFF;
-  Wire.write(myArray, 2);
+  //Serial.println(sensor_value0);
 
-  //Wire.write(sensor_value0);
-  //Wire.write("hilli\nsdfg"); // respond with message of 6 bytes
-  // as expected by master
+
+
+//
+//  myArray[0] = (sensor_value0 >> 8) & 0xFF;
+//  myArray[1] = sensor_value0 & 0xFF;
+//  Wire.write(myArray, 2);
+//
+//  myArray[0] = (sensor_value1 >> 8) & 0xFF;
+//  myArray[1] = sensor_value1 & 0xFF;
+//  Wire.write(myArray, 2);
+//  
+//  myArray[0] = (sensor_value2 >> 8) & 0xFF;
+//  myArray[1] = sensor_value2 & 0xFF;
+//  Wire.write(myArray, 2);
+//  
+//  myArray[0] = (sensor_value3 >> 8) & 0xFF;
+//  myArray[1] = sensor_value3 & 0xFF;
+//  Wire.write(myArray, 2);
 }
