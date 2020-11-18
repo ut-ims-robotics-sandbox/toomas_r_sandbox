bf0519838bf115ee858aa7ef9009e5658f0cc885
Toomas Roosma <toomas.roosma@gmail.com>
2020-11-18 12:36:50 +0200

    basic i2c working

--- master_right/master_right.ino
+++ master_right/master_right.ino
@@ -1,9 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
+// Wire Master Reader
+// by Nicholas Zambetti <http://www.zambetti.com>
 
+// Demonstrates use of the Wire library
+// Reads data from an I2C/TWI slave device
+// Refer to the "Wire Slave Sender" example for use with this
+
+// Created 29 March 2006
+
+// This example code is in the public domain.
+
+
+#include <Wire.h>
+
+const int addr = 8;
+const int n_bytes = 6;
+
+void setup() {
+  Wire.begin();        // join i2c bus (address optional for master)
+  Serial.begin(9600);  // start serial for output
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Wire.requestFrom(addr, n_bytes);    // request 6 bytes from slave device #8
 
+  while (Wire.available()) { // slave may send less than requested
+    char c = Wire.read(); // receive a byte as character
+    Serial.print(c);         // print the character
+  }
+
+  delay(500);
 }
--- slave_left/slave_left.ino
+++ slave_left/slave_left.ino
@@ -1,9 +1,19 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
 
+const int addr = 8;
+
+void setup() {
+  Wire.begin(addr);                // join i2c bus with address #8
+  Wire.onRequest(requestEvent); // register event
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(100);
+}
 
+// function that executes whenever data is requested by master
+// this function is registered as an event, see setup()
+void requestEvent() {
+  Wire.write("hilli\nsdfg"); // respond with message of 6 bytes
+  // as expected by master
 }
