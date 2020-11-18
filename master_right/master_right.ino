7996ab4389dd4a9b098f1a4f612299671bf23ab4
Toomas Roosma <toomas.roosma@gmail.com>
2020-11-18 13:15:23 +0200

    i2c capable of sending raw mesurements

--- master_right/master_right.ino
+++ master_right/master_right.ino
@@ -1,32 +1,26 @@
-// Wire Master Reader
-// by Nicholas Zambetti <http://www.zambetti.com>
-
-// Demonstrates use of the Wire library
-// Reads data from an I2C/TWI slave device
-// Refer to the "Wire Slave Sender" example for use with this
-
-// Created 29 March 2006
-
-// This example code is in the public domain.
-
-
 #include <Wire.h>
 
 const int addr = 8;
-const int n_bytes = 6;
-
+const int n_bytes = 8;
+byte a, b;
+int16_t bigNum;
+int16_t smallNum;
 void setup() {
   Wire.begin();        // join i2c bus (address optional for master)
   Serial.begin(9600);  // start serial for output
 }
 
 void loop() {
-  Wire.requestFrom(addr, n_bytes);    // request 6 bytes from slave device #8
+  Wire.requestFrom(addr, n_bytes);    // request 6 bytes from slave device addr
 
   while (Wire.available()) { // slave may send less than requested
-    char c = Wire.read(); // receive a byte as character
-    Serial.print(c);         // print the character
+    a = Wire.read();
+    b = Wire.read();
+    smallNum = a;
+    bigNum = smallNum << 8 | b;
+    Serial.print(bigNum);
+    Serial.print(" ");
   }
-
+  Serial.println();
   delay(500);
 }
--- slave_left/slave_left.ino
+++ slave_left/slave_left.ino
@@ -2,9 +2,22 @@
 
 const int addr = 8;
 
+int16_t sensor_value0;
+int16_t sensor_value1;
+int16_t sensor_value2;
+int16_t sensor_value3;
+
+byte myArray[2];
+
+
 void setup() {
-  Wire.begin(addr);                // join i2c bus with address #8
+  Wire.begin(addr);                // join i2c bus with address addr
   Wire.onRequest(requestEvent); // register event
+
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
 }
 
 void loop() {
@@ -14,6 +27,36 @@ void loop() {
 // function that executes whenever data is requested by master
 // this function is registered as an event, see setup()
 void requestEvent() {
-  Wire.write("hilli\nsdfg"); // respond with message of 6 bytes
+  //master küsib meilt infot, teostame mõõtmised ja vastame talle
+
+  sensor_value0 = analogRead(A0);
+  sensor_value1 = analogRead(A1);
+  sensor_value2 = analogRead(A2);
+  sensor_value3 = analogRead(A3);
+
+  sensor_value0 = 1023;
+  sensor_value1 = 103;
+  sensor_value2 = 800;
+  sensor_value3 = 987;
+
+
+  myArray[0] = (sensor_value0 >> 8) & 0xFF;
+  myArray[1] = sensor_value0 & 0xFF;
+  Wire.write(myArray, 2);
+
+  myArray[0] = (sensor_value1 >> 8) & 0xFF;
+  myArray[1] = sensor_value1 & 0xFF;
+  Wire.write(myArray, 2);
+  
+  myArray[0] = (sensor_value2 >> 8) & 0xFF;
+  myArray[1] = sensor_value2 & 0xFF;
+  Wire.write(myArray, 2);
+  
+  myArray[0] = (sensor_value3 >> 8) & 0xFF;
+  myArray[1] = sensor_value3 & 0xFF;
+  Wire.write(myArray, 2);
+
+  //Wire.write(sensor_value0);
+  //Wire.write("hilli\nsdfg"); // respond with message of 6 bytes
   // as expected by master
 }
